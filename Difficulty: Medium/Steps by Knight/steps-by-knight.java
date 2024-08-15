dd32d4dfd882e329cad227c6e72d1820ba68bfd0
Suraj Panda <64726057+Suraj370@users.noreply.github.com>
2024-08-15 23:02:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Steps by Knight/steps-by-knight.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int N = Integer.parseInt(br.readLine().trim());
+            String[] S1 = br.readLine().trim().split(" ");
+            String[] S2 = br.readLine().trim().split(" ");
+            int[] KnightPos = new int[2];
+            int[] TargetPos = new int[2];
+            for(int i = 0; i < 2; i++){
+                KnightPos[i] = Integer.parseInt(S1[i]);
+                TargetPos[i] = Integer.parseInt(S2[i]);
+            }
+            Solution obj = new Solution();
+            int ans = obj.minStepToReachTarget(KnightPos, TargetPos, N);
+            System.out.println(ans);
+       }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+       static class cell {
+        int x, y;
+        int dis;
+
+        public cell(int x, int y, int dis)
+        {
+            this.x = x;
+            this.y = y;
+            this.dis = dis;
+        }
+    }
+
+    // Utility method returns true if (x, y) lies
+    // inside Board
+    static boolean isInside(int x, int y, int N)
+    {
+        if (x >= 1 && x <= N && y >= 1 && y <= N)
+            return true;
+        return false;
+    }
+    //Function to find out minimum steps Knight needs to reach target position.
+    public int minStepToReachTarget(int knightPos[], int targetPos[], int N)
+    {
+        // Code here
+            // x and y direction, where a knight can move
+        int dx[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
+        int dy[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+
+        // queue for storing states of knight in board
+        Queue<cell> q = new LinkedList<>();
+
+        // push starting position of knight with 0 distance
+        q.add(new cell(knightPos[0], knightPos[1], 0));
+
+        cell t;
+        int x, y;
+        boolean visit[][] = new boolean
+            [N + 1][N + 1]; // default initialized to false
+
+        // visit starting state
+        visit[knightPos[0]][knightPos[1]] = true;
+
+        // loop until we have one element in queue
+        while (!q.isEmpty()) {
+            t = q.poll();
+
+            // if current cell is equal to target cell,
+            // return its distance
+            if (t.x == targetPos[0] && t.y == targetPos[1])
+                return t.dis;
+
+            // loop for all reachable states
+            for (int i = 0; i < 8; i++) {
+                x = t.x + dx[i];
+                y = t.y + dy[i];
+
+                // If reachable state is not yet visited and
+                // inside board, push that state into queue
+                if (isInside(x, y, N) && !visit[x][y]) {
+                    visit[x][y] = true;
+                    q.add(new cell(x, y, t.dis + 1));
+                }
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+}
