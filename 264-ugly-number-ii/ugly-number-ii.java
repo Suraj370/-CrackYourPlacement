02a2b54f985136de2bc176eea58893b0ad4f6194
Suraj Panda <64726057+Suraj370@users.noreply.github.com>
2024-08-23 08:10:06 +0530

    Time: 47 ms (41.21%) | Memory: 44.9 MB (7.17%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,28 +1,33 @@
+import java.util.*;
+
 class Solution {
     public int nthUglyNumber(int n) {
-        int[] uglyNumbers = new int[n];
-        uglyNumbers[0] = 1;
-
-        int index2 = 0, index3 = 0, index5 = 0;
-
-        for (int i = 1; i < n; ++i) {
-            int nextMultipleOf2 = uglyNumbers[index2] * 2;
-            int nextMultipleOf3 = uglyNumbers[index3] * 3;
-            int nextMultipleOf5 = uglyNumbers[index5] * 5;
-
-            uglyNumbers[i] = Math.min(nextMultipleOf2, Math.min(nextMultipleOf3, nextMultipleOf5));
-
-            if (uglyNumbers[i] == nextMultipleOf2) {
-                index2++;
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        Set<Long> seen = new HashSet<>();
+        
+        pq.offer(1L);
+        seen.add(1L);
+        
+        long current = 0;
+        for (int i = 0; i < n; i++) {
+            current = pq.poll();
+            
+            long next = current * 2;
+            if (seen.add(next)) {
+                pq.offer(next);
             }
-            if (uglyNumbers[i] == nextMultipleOf3) {
-                index3++;
+            
+            next = current * 3;
+            if (seen.add(next)) {
+                pq.offer(next);
             }
-            if (uglyNumbers[i] == nextMultipleOf5) {
-                index5++;
+            
+            next = current * 5;
+            if (seen.add(next)) {
+                pq.offer(next);
             }
         }
-
-        return uglyNumbers[n - 1];
+        
+        return (int) current;
     }
 }
